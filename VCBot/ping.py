4cfc227a1b074ff6b0a3a2d035e25e0fecc524a2
userderdead <96183105+userderdead@users.noreply.github.com>
2021-12-16 14:38:54 +0530

    Update ping.py

--- VCBot/ping.py
+++ VCBot/ping.py
@@ -9,3 +9,14 @@ from time import time
 from datetime import datetime
 
 # System Uptime
+START_TIME = datetime.utcnow()
+TIME_DURATION_UNITS = (
+    ('Week', 60 * 60 * 24 * 7),
+    ('Day', 60 * 60 * 24),
+    ('Hour', 60 * 60),
+    ('Min', 60),
+    ('Sec', 1)
+)
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
