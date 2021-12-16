41bb98a3b78cf5b388ee8ecdbef1ed6d3dbf31ea
userderdead <96183105+userderdead@users.noreply.github.com>
2021-12-16 14:35:17 +0530

    Update ping.py

--- VCBot/ping.py
+++ VCBot/ping.py
@@ -1,2 +1,11 @@
 import os
 import sys
+import asyncio
+from pyrogram import Client
+from pyrogram import filters
+from pyrogram.types import Message
+from config import bot, call_py, HNDLR, contact_filter
+from time import time
+from datetime import datetime
+
+# System Uptime
