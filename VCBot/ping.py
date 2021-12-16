f1d7da0879c2bf17fdd4f8df9efab319355b3524
userderdead <96183105+userderdead@users.noreply.github.com>
2021-12-16 14:47:01 +0530

    Update ping.py

--- VCBot/ping.py
+++ VCBot/ping.py
@@ -20,3 +20,24 @@ TIME_DURATION_UNITS = (
 async def _human_time_duration(seconds):
     if seconds == 0:
         return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+        return ', '.join(parts)
+    
+    
+@Client.on_message(contact_filter & filters.command(['ping'], prefixes=f"{HNDLR}"))
+async def ping(client, m: Message):
+    start = time()
+    current_time = datetime.utcnow()
+    m_reply = await m.reply_text("`...`")
+    delta_ping = time() - start
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await m_reply.edit(f"`{delta_ping * 1000:.3f} ms` \n**Standtime ⏳** - `{uptime}`")
+    
+    
+@Client.on_message(contact_filter & filters.command(['restart'], prefixes=f"{HNDLR}"))
