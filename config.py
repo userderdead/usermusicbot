22a9bd6dc5ee5e07811a18444734f90c61391412
Akku <96183105+userderdead@users.noreply.github.com>
2022-10-26 11:40:34 +0530

    Additional .end

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ if os.path.exists(".env"):
 # Necessary Vars
 API_ID = int(os.getenv("API_ID", "6"))
 API_HASH = os.getenv("API_HASH", "803b9bf62395f64a16a299fce45f3482")
-SESSION = os.getenv("SESSION", "session")
+SESSION = os.getenv("SESSION", "BQAhIHQAWbgUu4CNAyADUcc7HndXJrSZHOGm7e7hbuwyfdaNuFJOHw_AZwm9JG0XYyMAqLX99mMUMTmO-OMeU5wGiu-9AyJTmBeXske562ell7gnkQwkOdHkNS27Esw7n4lnss_HtUlwXCFFVhwcAmVqP0QYzg9c5b40frW8yhASRhB4YB0M8MWpbqPLfuih7dzZAogK0TSAu0xhQ3eNqTWy6J-RboWM46Mk_J4nzSZW-4V1uIkkqsFWg7dXMYjTfFyX4hHOA12XCCwAM_331cQWK_FZ8B5ooL-1w9kS5mIMyX1ViVYIrhcsfKVBGdTZ8hSqlVn0jl7MrSaro9xyWopfq_5s9gAAAAEqTn8OAA")
 HNDLR = os.getenv("HNDLR", ".")
 GROUP_MODE = os.getenv("GROUP_MODE", "True")
 
